6d382ccf510a0e3814997087fd998698cf83e7ca
Ian Luites <IanLuites@users.noreply.github.com>
2020-02-23 08:02:31 +0100

    Base project.

---
+++ mix.exs
@@ -0,0 +1,68 @@
+defmodule Kvasir.Postgres.MixProject do
+  use Mix.Project
+
+  @version "1.0.0"
+
+  def project do
+    [
+      app: :kvasir_postgres,
+      version: "0.0.1",
+      description: "PostgreSQL based cold storage for Kvasir.",
+      version: @version,
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+
+      # Testing
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [ignore_warnings: ".dialyzer", plt_add_deps: true],
+
+      # Docs
+      name: "Kvasir PostgreSQL",
+      source_url: "https://github.com/IanLuites/kvasir_postgres",
+      homepage_url: "https://github.com/IanLuites/kvasir_postgres",
+      docs: [
+        main: "readme",
+        extras: ["README.md"],
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/IanLuites/kvasir_postgres"
+      ]
+    ]
+  end
+
+  def package do
+    [
+      name: :kvasir_postgres,
+      maintainers: ["Ian Luites"],
+      licenses: ["MIT"],
+      files: [
+        # Elixir
+        "lib/kvasir",
+        ".formatter.exs",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/IanLuites/kvasir_postgres"
+      }
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:postgrex, "~> 0.15.1"}
+    ]
+  end
+end
